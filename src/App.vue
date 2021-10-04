4fc1161fc7bcd334760f651f988181a075ee5854
Rayane Queiroz <rayanequeirozrj@gmail.com>
2021-10-04 19:46:57 -0300

    Atualizando projeto

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,95 @@
 <template>
-  <v-app>
-   
+<v-app>
+<v-navigation-drawer
+  v-model="drawer"
+  app>
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="text-h6">
+            Vue.js
+          </v-list-item-title>
+          <v-list-item-subtitle>
+            Lista de Tarefas
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list
+        dense
+        nav
+      >
+        <v-list-item
+          v-for="item in items"
+          :key="item.title"
+          :to="item.to"
+          link
+        >
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar
+      app
+      color="#fcb69f"
+      dark
+      prominent
+      src="./assets/bg.jpg"
+    >
+      <template v-slot:img="{ props }">
+        <v-img
+          v-bind="props"
+          gradient="to top right, rgba(19,84,122,.5), rgba(128,208,199,.8)"
+        ></v-img>
+      </template>
+
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-app-bar-title>To do</v-app-bar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-dots-vertical</v-icon>
+      </v-btn>
+    </v-app-bar>
+
     <v-main>
-    
+      <router-view/>
     </v-main>
-  </v-app>
+
+</v-app>
 </template>
 
+
 <script>
-
 export default {
-  name: 'App',
-
-  components: {
-    //
-  },
-
   data: () => ({
-    //
+    drawer: true,
+    items: [
+      { title: "Tarefas", icon: 'mdi-view-dashboard', to:'/'},
+      {title: "Sobre", icon: 'mdi-help-box', to:'/sobre'}
+    ],
   }),
-};
+}
 </script>
+
+
+<style scoped>
+</style>
---
+++ src/views/Tarefas.vue
@@ -0,0 +1,72 @@
+<template>
+  <div>
+    <v-col
+      cols="12"
+        >
+    <v-text-field
+      v-model="campoInput"
+      label="Qual sua tarefa?"
+      outlined
+      clearable
+      @keyup.enter="handleAddTarefa"
+    ></v-text-field>
+        </v-col>
+
+    <v-list
+      flat
+      subheader
+    >
+
+      <v-list-item-group
+        multiple
+        active-class=""
+      >
+      <div
+      v-for="tarefa,index in tarefas"
+      :key="index">
+      <Tarefa
+      :tarefa="tarefa"
+      />
+      </div>
+
+   
+
+
+      </v-list-item-group>
+    </v-list>
+    
+  </div>
+</template>
+
+<script>
+import Tarefa from '../components/tarefas/Tarefa.vue'
+
+export default {
+  name: 'Home',
+
+components: {
+  Tarefa
+  },
+  data() {
+    return{
+      campoInput: null,
+
+      tarefas:[
+        {titulo:"Ir ao mercado", concluido:false},
+        {titulo:"Comprar ração", concluido:false},
+      ]
+    }
+  },
+  methods:{
+    handleAddTarefa(){
+      if(this.campoInput){
+        this.tarefas.push({
+          titulo: this.campoInput,
+          concluido: false
+        })
+        this.campoInput = null
+      }
+    }
+  }
+}
+</script>
